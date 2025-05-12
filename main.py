920b69b83ad783750f63831dc5424266e8827bbc
David Beltran <beltrannowd5@gmail.com>
2025-05-12 13:19:23 -0700

    reached maxtry error

--- main.py
+++ main.py
@@ -8,26 +8,20 @@ vowels = r'^[aeiouAEIOU]\w*'
 
 tickers = []
 count = 0
-for t in client.list_tickers(
-	market="stocks",
-	active="true",
-	order="asc",
-	limit="100",
-	sort="ticker",
-	):
-    tickers.append(t)
-    count += 1
-    time.sleep(.1)
-    if count > 10:
-        break
+# for t in client.list_tickers(
+# 	market="stocks",
+# 	active="true",
+# 	order="asc",
+# 	limit="100",
+# 	sort="ticker",
+# 	):
+#     tickers.append(t)
+#     count += 1
+#     time.sleep(.1)
+#     if count > 10:
+#         break
     
-matches = [s for s in client.list_tickers(
-	market="stocks",
-	active="true",
-	order="asc",
-	limit="100",
-	sort="ticker",
-	): if re.match(vowels, s)]
+matches = [s for s in client.list_tickers(market="stocks",active="true",order="asc",limit="100",sort="ticker",) if re.match(vowels, s.ticker)]
 print(matches)
 # pprint.pprint(tickers[8].ticker)
 
